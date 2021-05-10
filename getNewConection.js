a6fcfb6ac7bae8fe483866109f16797a4b793bed
jsBoy-dnr <kryptex.studio@yandex.ru>
2021-05-10 15:23:34 +0300

    first

--- getNewConection.js
+++ getNewConection.js
@@ -3,10 +3,16 @@ const proxyChain = require('proxy-chain')
 const citySelect = require('./citySelect.js')
 function getProxy() {
     let arr = [
-        'http://ZDRrbv:bRUXRN@213.166.75.200:9758',
-        'http://ZDRrbv:bRUXRN@213.166.75.244:9758',
-        'http://ZDRrbv:bRUXRN@213.166.72.205:9484',
-        'http://ZDRrbv:bRUXRN@213.166.73.167:9528'
+        'http://P8B1byap:5iRWcGrQ@92.249.15.237:54443',
+        'http://P8B1byap:5iRWcGrQ@45.129.79.36:63811',
+        'http://P8B1byap:5iRWcGrQ@2.56.138.228:61735',
+        'http://P8B1byap:5iRWcGrQ@45.131.47.233:45945',
+        'http://P8B1byap:5iRWcGrQ@45.148.242.159:48602',
+        'http://P8B1byap:5iRWcGrQ@45.132.129.146:51231',
+        'http://P8B1byap:5iRWcGrQ@45.139.52.29:50887',
+        'http://P8B1byap:5iRWcGrQ@91.188.231.136:60840',
+        'http://P8B1byap:5iRWcGrQ@194.156.104.189:59736',
+        'http://P8B1byap:5iRWcGrQ@91.199.112.55:48734',
     ]
     return arr[Math.floor(Math.random()*arr.length)]
 }
