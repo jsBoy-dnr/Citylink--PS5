eaef310c45eea93f4c68854a619c9f3aaf64b5e1
jsBoy-dnr <kryptex.studio@yandex.ru>
2021-05-11 13:44:00 +0300

    last

--- getNewConection.js
+++ getNewConection.js
@@ -25,7 +25,7 @@ async function getNewConection(gotolink, city) {
     const newProxyUrl = await proxyChain.anonymizeProxy(getProxy())
     let browser = await puppeteer.launch({
         ignoreHTTPSErrors: true,
-        headless: false,
+        headless: true,
         slowMo: 300,
         devtools: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox', `--proxy-server=${newProxyUrl}`]
